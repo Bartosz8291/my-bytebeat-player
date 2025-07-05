35f41b0fc2e29b08c34246dab1e55e94ee0a7158
BARTEKK23 <164915721+Bartosz8291@users.noreply.github.com>
2025-07-05 11:57:50 +0200

    Update server.ts

--- api/server.ts
+++ api/server.ts
@@ -1,13 +1,13 @@
-import express, { Request, Response } from "express";
-import path from "path";
-import createServer from "vercel-node-server";
+const express = require('express');
+const path = require('path');
+const createServer = require('vercel-node-server');
 
 const app = express();
 
-app.use(express.static(path.join(process.cwd(), "public")));
+app.use(express.static(path.join(process.cwd(), 'public')));
 
-app.get("/", (_req: Request, res: Response) => {
-  res.sendFile(path.join(process.cwd(), "public", "index.html"));
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
 });
 
-export default createServer(app);
+module.exports = createServer(app);
